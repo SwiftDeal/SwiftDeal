03c6525d401d38d9649c723a06f96e0427071de4
faizanayubi <indianayubi@gmail.com>
2015-05-16 14:03:25 +0530

    Cron Added

---
+++ cron/mail_notfound.php
@@ -0,0 +1,18 @@
+<?php
+	include '../includes/initialize.php';
+
+	$founds = Found::find_all();
+	foreach($founds as $found){
+		$results = title_search($found->keywords);
+		if(!empty($results)){
+			foreach($results as $result){
+				if($found->active){
+					$found->send_notification($found->id, $result['id']);
+					$found->mail_counter($found->id);
+					log_action("Item Found", "For {$found->keywords} to {$found->email} at {$created}");
+				}
+			}
+		}
+	}
+	
+?>
---
+++ cron/weekly.php
@@ -0,0 +1,11 @@
+<?php
+include '../includes/initialize.php';
+$items = Item::find_all();
+foreach($items as $item){
+	if($item->weekly_summaries()){
+		$item->weekly_mail_count($item->id);
+		log_action("Weekly Summaries", "For {$item->title} to {$item->email} at {$item->created}");
+	}
+}
+
+?>
